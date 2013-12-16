f1fa43f1adf1d18142300b38585feb176417178a
Douglas Kuhn <bluewitchproductions@gmail.com>
2013-12-16 12:18:04 -0800

    Create game.js
    Partial Code for game.js

---
+++ game.js
@@ -0,0 +1,42 @@
+// game.js
+// A sample game program
+
+
+// Gloabal Variables
+var Player0;
+var Player1;
+
+var STR = 0; // Strength
+var AGL = 0; // Agility
+var STA = 0; // Stanima
+
+var INT = 0; // Intel
+var PER = 0; // Perception
+var WIL = 0; // Willpower
+
+var CHA = 0; // Charisma
+var BEA = 0; // Beauty
+
+
+Character = new Object;
+Character.name =   "";  // name of the character
+Character.race =   "";  // race of the character
+Character.sex  =   "";  // biological sex
+Character.gender = "";  // mental gender orientation
+
+
+
+Sex = new Object;
+Sex.genitalia = "";
+Sex.secondary = "";
+
+
+
+function buildCharacter(){
+
+};
+
+function showCharacter(){
+
+};
+
