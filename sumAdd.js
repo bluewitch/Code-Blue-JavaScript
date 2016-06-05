d2ec4659f73272e7e86abfd40289ed5c7caf35e2
Douglas Kuhn <bluewitchproductions@gmail.com>
2016-06-05 09:49:42 -0700

    Up-date code
    How to find the sum of an array of numbers

--- sumAdd.js
+++ sumAdd.js
@@ -19,3 +19,9 @@ for (i = 9; i < 10; i++)
 {
     document.write("The sum of all the elements is: " + /* problem here */ + " The average of all the elements is: " + /* problem here */ + "<br/>");
 }  
+
+
+// How to find the sum of an array of numbers
+// ECMAScript 2015 (aka ECMAScript 6)
+var sum = [1, 2, 3].reduce((a, b) => a + b, 0);
+console.log(sum); // 6
