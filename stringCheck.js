9db6a72b76e988c2a5a594d23a13399f2748b292
Douglas Kuhn <bluewitchproductions@gmail.com>
2016-06-06 09:22:22 -0700

    Update stringCheck.js
    Just identifying return as true or false

--- stringCheck.js
+++ stringCheck.js
@@ -1,6 +1,7 @@
 /*
 Javascript function to check if a String
-has upper and lower case
+has upper and lower case; string enters
+the str parameter and returns true or false
 */
 function stringCheck(str) {
     return str.match(/[a-z]/) && str.match(/[A-Z]/);
